d1810ae72de3203cc83db09a711f23b99f5675fc
HelgeArkitektum <helge@arkitektum.no>
2022-01-05 21:13:33 +0100

    Added basis endpoint

---
+++ Models/CalculationResultBasis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ATIL.FeeCalculator.Models
+{
+    public class CalculationResultBasis
+    {
+        [JsonPropertyName("gebyrbeloep")]
+        public int FeeAmount { get; set; }
+        [JsonPropertyName("gebyrkategori")]
+        public string FeeCategory { get; set; }
+    }
+}
