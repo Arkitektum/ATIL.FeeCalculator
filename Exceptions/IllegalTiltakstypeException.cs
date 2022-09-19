67bdc8aeaf979a91bbc78d5d45bb8d05f70451b1
HelgeArkitektum <helge@arkitektum.no>
2022-09-19 14:52:14 +0200

    Only one tiltakstype has to be correct

---
+++ Exceptions/IllegalTiltakstypeException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ATIL.FeeCalculator.Exceptions
+{
+    [Serializable]
+    public class IllegalTiltakstypeException : Exception
+    {
+        public IllegalTiltakstypeException()
+        {
+        }
+
+        public IllegalTiltakstypeException(string message) : base(message)
+        {
+        }
+
+        public IllegalTiltakstypeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        //protected IllegalTiltakstypeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        //{
+        //}
+    }
+}
