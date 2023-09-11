a9753edffbb257aef8f9aea1e1e9b3efbaf97db4
Øystein Thoen Sisjord <oystein@arkitektum.no>
2023-09-11 13:28:50 +0200

    Bygg til dev

--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Builder;
 
 namespace ATIL.FeeCalculator
 {
